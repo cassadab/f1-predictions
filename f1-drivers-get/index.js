ec3e124c78deff78dcd0dd3208b37f692083e6fd
Brady Cassada <bradycassada@gmail.com>
2022-03-12 19:29:33 +0000

    drivers get lambda

---
+++ f1-drivers-get/index.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const rdsDataService = new AWS.RDSDataService();
+
+exports.handler = async (event) => {
+    const sqlParams = {
+        secretArn: process.env.SECRET_ARN,
+        resourceArn: process.env.CLUSTER_ARN,
+        sql: 'select * from drivers',
+        database: 'f1_predictions',
+    };
+    const result = await rdsDataService.executeStatement(sqlParams).promise();
+    
+    const parsedRecords = result.records.map((record) => parseRecord(record));
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+        },
+        body: parsedRecords,
+    };
+    
+    return response;
+};
+
+function parseRecord(record) {
+    return {
+        'id': record[0].longValue,
+        'name': record[1].stringValue,
+        'team': record[2].stringValue,
+        'rank': record[3].longValue || null,
+        'coutry': record[4].stringValue,
+    };
+}
