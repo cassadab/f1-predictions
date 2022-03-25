f88051d5c64cdfbc9d897a3d4680b728aa8d93d4
Brady Cassada <bradycassada@gmail.com>
2022-03-24 21:36:28 -0500

    refactor predictions get to use scores lambda

--- f1-predictions-get/.eslintrc.js
+++ f1-predictions-get/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"]
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'max-len': ['error', { code: 120 }],
+    indent: ['error', 2],
+    'newline-per-chained-call': 'off',
+  },
 };
-
