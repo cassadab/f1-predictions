a20f45f9799f2e305a0503e399c64251902a2821
Brady Cassada <bradycassada@gmail.com>
2022-03-30 22:26:59 -0500

    create public subnet and tweak rds

---
+++ infrastructure/lambda_sandbox.tf
@@ -0,0 +1,20 @@
+module "sandbox_lambda" {
+  source = "./modules/lambda"
+
+  lambda_name = "f1-sandbox"
+  description = "Retrieve list of predictions"
+  acc_number  = var.acc_number
+  timeout     = 10
+  rds_config = {
+    required       = true
+    connect_policy = aws_iam_policy.beeg_yoshi_rds_connect.name
+    endpoint       = aws_db_instance.beeg_yoshi_f1.endpoint
+    instance_arn   = aws_db_instance.beeg_yoshi_f1.arn
+    user           = var.db_lambda_user
+  }
+  vpc_config = {
+    required           = true
+    subnet_ids         = [aws_subnet.zone1.id, aws_subnet.zone2.id]
+    security_group_ids = [aws_security_group.beeg_yoshi_f1.id]
+  }
+}
