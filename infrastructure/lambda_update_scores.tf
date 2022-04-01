15c8fa525260d41ff026516a4cb9f261b456333d
Brady Cassada <bradycassada@gmail.com>
2022-04-01 00:02:46 -0500

    lambda public net access

--- infrastructure/lambda_update_scores.tf
+++ infrastructure/lambda_update_scores.tf
@@ -5,6 +5,18 @@ module "update_scores_lambda" {
   description = "Update driver standings and prediction rankings"
   acc_number  = var.acc_number
   timeout     = 10
+  rds_config = {
+    required       = true
+    connect_policy = aws_iam_policy.beeg_yoshi_rds_connect.arn
+    endpoint       = aws_db_instance.beeg_yoshi_f1.endpoint
+    instance_arn   = aws_db_instance.beeg_yoshi_f1.arn
+    user           = var.db_lambda_user
+  }
+  vpc_config = {
+    required           = true
+    subnet_ids         = [aws_subnet.zone1.id, aws_subnet.zone2.id]
+    security_group_ids = [aws_security_group.beeg_yoshi_f1.id, aws_security_group.public_internet.id]
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "update_scores_invoke" {
--- infrastructure/vpc_public_lambda.tf
+++ infrastructure/vpc_public_lambda.tf
@@ -1,8 +1,6 @@
 resource "aws_route_table" "public_lambda" {
   vpc_id = aws_vpc.beeg_yoshi_f1.id
 
-  route = []
-
   tags = {
     Name = "BeegYoshi Lambda Public"
   }
@@ -13,10 +11,15 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public_lambda.id
 }
 
+# Direct all other traffic that is not staying in the subnet
+resource "aws_route" "default_public_route" {
+  route_table_id         = aws_route_table.public_lambda.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.lambda_gw.id
+}
+
 resource "aws_route_table" "private_lambda" {
   vpc_id = aws_vpc.beeg_yoshi_f1.id
-
-  route = []
 
   tags = {
     Name = "BeegYoshi Lambda Private"
@@ -29,8 +32,15 @@ resource "aws_route_table_association" "private_zone_1" {
 }
 
 resource "aws_route_table_association" "private_zone2" {
-  subnet_id      = aws_subnet.zone1.id
+  subnet_id      = aws_subnet.zone2.id
   route_table_id = aws_route_table.private_lambda.id
+}
+
+# Direct all other traffic that is not staying in the subnet
+resource "aws_route" "default_private_route" {
+  route_table_id         = aws_route_table.private_lambda.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.beeg_yoshi_f1.id
 }
 
 resource "aws_internet_gateway" "lambda_gw" {
@@ -40,3 +50,11 @@ resource "aws_internet_gateway" "lambda_gw" {
     Name = "BeegYoshi Lambda"
   }
 }
+
+resource "aws_nat_gateway" "beeg_yoshi_f1" {
+  allocation_id     = aws_eip.public_lambda.id
+  subnet_id         = aws_subnet.public.id
+  connectivity_type = "public"
+}
+
+resource "aws_eip" "public_lambda" {}
