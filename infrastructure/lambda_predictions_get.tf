3a6db7699db9ba9cbb340977315e537cecd106eb
Brady Cassada <bradycassada@gmail.com>
2022-04-20 21:45:55 -0500

    remove rds and dependencies

--- infrastructure/lambda_predictions_get.tf
+++ infrastructure/lambda_predictions_get.tf
@@ -1,24 +1,3 @@
-module "predictions_get_lambda" {
-  source = "./modules/lambda"
-
-  lambda_name = "f1-predictions-get"
-  description = "Retrieve prediction details"
-  acc_number  = var.acc_number
-  timeout     = 10
-  rds_config = {
-    required       = true
-    connect_policy = aws_iam_policy.beeg_yoshi_rds_connect.arn
-    endpoint       = aws_db_instance.beeg_yoshi_f1.endpoint
-    instance_arn   = aws_db_instance.beeg_yoshi_f1.arn
-    user           = var.db_lambda_user
-  }
-  vpc_config = {
-    required           = true
-    subnet_ids         = [aws_subnet.zone1.id, aws_subnet.zone2.id]
-    security_group_ids = [aws_security_group.beeg_yoshi_f1.id]
-  }
-}
-
 module "predictions_get_dev_lambda" {
   source = "./modules/lambda"
 
